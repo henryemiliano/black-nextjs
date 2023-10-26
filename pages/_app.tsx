37ee4a5f759020395ab732173c9ef1dc051b2e4d
henryemiliano <henry.emiliano@hotmail.com>
2023-10-26 20:22:50 -0300

    Aplicação Finalizada!

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import { CartContextProvider } from "@/hooks/useCart";
+import "bootstrap/dist/css/bootstrap.min.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <CartContextProvider>
+      <Component {...pageProps} />;
+    </CartContextProvider>
+  );
+}
---
+++ pages/cart.tsx
@@ -0,0 +1,33 @@
+// pages/cart.tsx
+
+import { NextPage } from "next";
+import Head from "next/head";
+import { Container } from "reactstrap";
+import Header from "../src/components/Header";
+import CartTable from "../src/components/CartTable";
+import CartTotal from "../src/components/CartTotal";
+
+const Cart: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Carrinho</title>
+        <meta name="description" content="Meu carrinho de compras" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <main>
+        <Container className="mb-5">
+          <h1 className="my-5">Carrinho</h1>
+
+          <CartTable />
+          <CartTotal />
+        </Container>
+      </main>
+    </>
+  );
+};
+
+export default Cart;
